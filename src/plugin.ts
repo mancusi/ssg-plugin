bfad2415e1f20099b444a0c9aeff023827e0f34d
Justin Mancusi <jmancusi@yext.com>
2022-03-14 15:05:21 -0400

    put server bundles in server/ dir

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,10 +1,31 @@
-import {Plugin} from "rollup"
+import {Plugin} from "vite";
 import buildStart from "./buildStart/buildStart";
 import buildEnd from "./buildEnd/buildEnd";
+import { readdir } from "fs/promises";
+import { parse } from "path";
+
 
 const plugin = (): Plugin => {
   return {
     name: "yext-sites-ssg",
+    config: async (config) => {
+      const dir = await readdir("./src/templates");
+      const input = {};
+      for (const template of dir) {
+        const parsedPath = parse(template);
+        input[`server/${parsedPath.name}`] = `./src/templates/${template}`;
+      }
+
+      return {
+        build: {
+          manifest: true,
+          rollupOptions: {
+            preserveEntrySignatures: "strict",
+            input,
+          },
+        },
+      };
+    },
     buildStart,
     buildEnd,
   };
